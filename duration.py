93bfe15476078816edce388fc8b674a74ca5caa6
harveensinghchadha@gmail.com <harveensinghchadha@gmail.com>
2020-12-16 12:01:09 +0000

    Harveen | Updating Recursive Duration

--- duration.py
+++ duration.py
@@ -6,19 +6,28 @@ from tqdm import tqdm
 import os
 from joblib import Parallel, delayed
 
-def get_duration_single_file(file):
-    return sox.file_info.duration(file)
+
+
+def get_duration_single_file(local_file):
+    duration = sox.file_info.duration(local_file)
+    clean_duration = 0    
+    if 'clean' in local_file.split('/'):
+        clean_duration = duration
+
+    return [duration, clean_duration]
 
 def get_duration(path_local):
     if not os.path.exists(path_local):
         raise Exception("Sorry this path doesn't exists")
-    files = glob.glob(path_local + '/*.wav')
+    files = glob.glob(path_local + '/**/*.wav', recursive=True)
     print("Number of files present: ", len(files))
     total_seconds = []
 
-    total_seconds.extend( Parallel(n_jobs=-1)( delayed(get_duration_single_file)(local_file) for local_file in tqdm(files) ) )
-    
-    print("Total number of data in hours: ", np.sum(total_seconds)/3600)
+    total_seconds.extend( Parallel(n_jobs=24)( delayed(get_duration_single_file)(local_file) for local_file in tqdm(files)))
+    total_ = np.sum(total_seconds, axis=0)
+    print(total_)
+    print("Total number of data in hours: ", total_[0]/3600)
+    print("Total number of clean data in hours: ", total_[1]/3600)
 
 if __name__ == "__main__":
     path = sys.argv[1]
