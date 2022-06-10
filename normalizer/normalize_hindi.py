f68a01d35530440bbad8654e2db762d6d6deb446
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2022-06-10 19:40:48 +0530

    Update normalize_hindi.py

--- normalizer/normalize_hindi.py
+++ normalizer/normalize_hindi.py
@@ -4,8 +4,9 @@ from indicnlp.normalize.indic_normalize import IndicNormalizerFactory
 lang = 'hi'
 normalizer = IndicNormalizerFactory().get_normalizer(lang)
 
-def normalize_text(sent):
+def normalize_hindi_text(sent):
     normalized = normalizer.normalize(sent)
     text = ' '.join(trivial_tokenize(normalized, lang))
     text=text.replace('\u090d','\u090f'+ '\u0901') # Custom rule 1 for ऍ
+    text=text.replace('\u0960','\u090b') # Custom rule 2 for ॠ
     return text
